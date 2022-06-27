c49f3410459b408cdeae43129e978f8bc7ba075e
Christian <capv5001@gmail.com>
2022-06-26 21:45:10 -0500

    feat(nombre)

--- header.php
+++ header.php
@@ -4,5 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo(basename($_SERVER['PHP_SELF'], ".php"))?></title>
+    <title>Hola</title>
 </head>
+<body>
+    <h1>prueba</h1>
+</body>
